a885157460efdb9a56bf00759631a68c4ee98727
Prisha Gupta <prishag407@gmail.com>
2026-04-23 10:40:47 +0800

    add music and photos

---
+++ photoapp/AddMusic.swift
@@ -0,0 +1,18 @@
+//
+//  AddMusic.swift
+//  photoapp
+//
+//  Created by Prisha on 23/04/26.
+//
+
+import SwiftUI
+
+struct AddMusic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMusic()
+}
---
+++ photoapp/PhotoPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPickerView.swift
+//  photoapp
+//
+//  Created by Prisha on 23/04/26.
+//
+
+import SwiftUI
+
+struct PhotoPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPickerView()
+}
